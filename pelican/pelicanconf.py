f480a64161b04f1292f20d06e395a99bb4e597b9
Richard Cornish <richardcornish@gmail.com>
2014-11-05 10:36:42 -0600

    Added custom domain

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -33,3 +33,7 @@ DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Custom domain
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
