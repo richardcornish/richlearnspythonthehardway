cef553a8606ad11bdf5f930b5af868ea528a5a36
Richard Cornish <richardcornish@gmail.com>
2014-11-05 13:20:20 -0600

    Edited Python-relevant blogroll links

--- pelican/pelicanconf.py
+++ pelican/pelicanconf.py
@@ -20,10 +20,8 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Python', 'http://python.org/'),
+         ('LPTHW', 'http://learnpythonthehardway.org/book/'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
