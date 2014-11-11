53a4da0e9b97e1aaeb1cedeaa1e47d00a611e85b
Richard Cornish <richardcornish@gmail.com>
2014-11-11 01:34:00 -0600

    Added exercise 5

---
+++ exercises/ex05.py
@@ -0,0 +1,33 @@
+# Exercise 5
+# http://learnpythonthehardway.org/book/ex5.html
+
+name = 'Zed A. Shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight = 180 # lbs
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+print "Let's talk about %s." % name
+print "He's %d inches tall." % height
+print "He's %d pounds heavy." % weight
+print "Actually that's not too heavy."
+print "He's got %s eyes and %s hair." % (eyes, hair)
+print "His teeth are usually %s depending on the coffee." % teeth
+
+# this line is tricky, try to get it exactly right
+print "If I add %d, %d, and %d I get %d." % (
+    age, height, weight, age + height + weight)
+
+
+# Study drills
+height_cm = height * 2.54 # cm
+weight_kg = weight * 0.453592 # kg
+
+print "He's %d centimeters tall." % height_cm
+print "He's %d kilograms heavy." % weight_kg
+
+# %r format character
+r = "Print this no matter what"
+print "Here is what %%r does: %r" % r
