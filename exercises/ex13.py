b4b93c7a530af35e3fe2d85834ccec241fb8004f
Richard Cornish <richardcornish@gmail.com>
2014-11-20 00:26:35 -0600

    Added exercise 13

---
+++ exercises/ex13.py
@@ -0,0 +1,14 @@
+# Exercise 13
+# http://learnpythonthehardway.org/book/ex13.html
+
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called:", script
+print "Your first variable is:", first
+print "Your second variable is:", second
+print "Your third variable is:", third
+
+opinion = raw_input("What do you think of this script? ")
+print "You think %s is %s!" % (script, opinion)
